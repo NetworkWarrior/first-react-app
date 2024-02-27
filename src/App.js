c85e25c1298adf8ce6c6cbaa730e9f8dfdf301dd
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-02-27 16:18:09 +0100

    dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+  const title = "welcome to the new blog";
+  const likes = 50;
+  const link = "http://www.google.com"
   return (
     <div className="App">
      <div className='content'>
-      <h1>App component</h1>
+      <h1>{title}</h1>
+      <p>liked {likes} times</p>
+      <a href={link}>google site</a>
      </div>
     </div>
   );
